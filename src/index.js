6b6b1ff73515e578b9f471e3f042885b54d40b20
vasilievi <vasilievi@outlook.com>
2022-11-13 17:55:40 +0300

    -

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 import ReactDOM from 'react-dom/client';
-import Main from "./pages/Main";
+
+import {
+    Routes,
+    Route,
+    BrowserRouter,
+} from "react-router-dom";
+
+// Components
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+// Contents
+import Dashboard from "./contents/Dashboard";
+import Tasks from "./contents/Tasks";
+import Profile from "./contents/Profile";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main />);
+root.render(
+    <BrowserRouter>
+        <div className="wrapper">
+            <Sidebar />
+            <div className="main">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/tasks" element={<Tasks />} />
+                    <Route path="/profile" element={<Profile />} />
+                </Routes>
+                <Footer />
+            </div>
+        </div>
+    </BrowserRouter>
+);
